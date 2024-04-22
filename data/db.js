9672d84905c2fd621b0fead494616d5294e8287c
julord87 <julianmzg@gmail.com>
2024-04-22 10:25:59 -0300

    april 2024

---
+++ data/db.js
@@ -0,0 +1,50 @@
+export const ochoproyectos = [
+    {
+        id: "img1",
+        nombre: "Proyecto 1",
+        descripcion: "Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza cl´sica de la literatura del Latin.",
+        imagen: "../images/1.jpg",
+    },
+    {
+        id: "img2",
+        nombre: "Proyecto 2",
+        descripcion: "Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza clésica de la literatura del Latin.",
+        imagen: "../images/2.jpg",
+    },
+    {
+        id: "img3",
+        nombre: "Proyecto 3",
+        descripcion: "Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza clésica de la literatura del Latin.",
+        imagen: "../images/3.jpg",
+    },
+    {
+        id: "img4",
+        nombre: "Proyecto 4",
+        descripcion: "Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza clésica de la literatura del Latin.",
+        imagen: "../images/4.jpg",
+    },
+    {
+        id: "img5",
+        nombre: "Proyecto 5",
+        descripcion: "Al método del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza clésica de la literatura del Latin.",
+        imagen: "../images/5.jpg",
+    },
+    {
+        id: "img6",
+        nombre: "Proyecto 6",
+        descripcion: "Al método del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza clésica de la literatura del Latin.",
+        imagen: "../images/6.jpg",
+    },
+    {
+        id: "img7",
+        nombre: "Proyecto 7",
+        descripcion: "Al método del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza clésica de la literatura del Latin.",
+        imagen: "../images/7.jpg",
+    },
+    {
+        id: "img8",
+        nombre: "Proyecto 8",
+        descripcion: "Al método del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza clésica de la literatura del Latin.",
+        imagen: "../images/bg2.jpg",
+    },
+]
