e448a52e5509d6ad36eb4413be4b12fd34ac5213
julord87 <julianmzg@gmail.com>
2024-11-15 11:26:07 -0300

    Update styles and project descriptions in CSS, HTML, and JS files

--- data/db.js
+++ data/db.js
@@ -6,21 +6,15 @@ export const ochoproyectos = [
         imagen: "../images/1.jpg",
     },
     {
-        id: "img2",
-        nombre: "Proyecto 2",
-        descripcion: "Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza clésica de la literatura del Latin.",
-        imagen: "../images/2.jpg",
-    },
-    {
         id: "img3",
-        nombre: "Proyecto 3",
-        descripcion: "Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza clésica de la literatura del Latin.",
+        nombre: "Sanatorio Banco de Seguros - 2015",
+        descripcion: "Sanatorio y Centro Nacional de Rehabilitación del Banco de Seguros del Estado. Concurso de anteproyecto MENCIÓN HONORÍFICA. Lugar: Montevideo, Uruguay. En conjunto con TEYMA. 15000m2. Año: 2015",
         imagen: "../images/3.jpg",
     },
     {
         id: "img4",
-        nombre: "Proyecto 4",
-        descripcion: "Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza clésica de la literatura del Latin.",
+        nombre: "Sanatorio Banco de Seguros - 2015",
+        descripcion: "Sanatorio y Centro Nacional de Rehabilitación del Banco de Seguros del Estado. Concurso de anteproyecto MENCIÓN HONORÍFICA. Lugar: Montevideo, Uruguay. En conjunto con TEYMA. 15000m2. Año: 2015",
         imagen: "../images/4.jpg",
     },
     {
@@ -31,13 +25,13 @@ export const ochoproyectos = [
     },
     {
         id: "img6",
-        nombre: "Proyecto 6",
-        descripcion: "Al método del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza clésica de la literatura del Latin.",
+        nombre: "Vestuarios de personal - Hosp. Maciel - 2019",
+        descripcion: "Reforma y reagrupación de los antiguos vestuarios de personal, donde optimizamos el área disponible de un tanque de abastecimiento abandonando y del vestuario masculino anterior, para generar en la misma área dos nuevos sectores: masculino y femenino. A tales efectos, se propuso un sector mixto destinado al aseo, con piletas y espejo.",
         imagen: "../images/6.jpg",
     },
     {
         id: "img7",
-        nombre: "Proyecto 7",
+        nombre: "Laboratorio de Oncología Molecular - Hosp. Maciel",
         descripcion: "Al método del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza clésica de la literatura del Latin.",
         imagen: "../images/7.jpg",
     },
